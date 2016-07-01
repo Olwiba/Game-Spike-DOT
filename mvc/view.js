a3c231b5d2ce134e17402d308316aa3b56bf9db5
Troy Lamerton <email": null>
2016-07-01 13:58:58 +1200

    Add basic structure of view.js

--- mvc/view.js
+++ mvc/view.js
@@ -1,3 +1,16 @@
 export default class View {
 
+  var canvas = document.getElementById("canvas")
+  var ctx = canvas.getContext("2d")
+
+  function clearCanvas () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  function drawPlayer (player) {
+    clearCanvas()
+    // draw player on canvas
+  }
+
+
 }

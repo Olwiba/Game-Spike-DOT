8ccda8ecc3180085b4f8ddb9742b48e73507416b
danielkermode <danielkermode@hotmail.co.nz>
2016-07-01 14:07:02 +1200

    proper class syntax view

--- mvc/view.js
+++ mvc/view.js
@@ -1,7 +1,8 @@
 export default class View {
-
-  static canvas = document.getElementById("canvas");
-  static ctx = canvas.getContext("2d");
+  constructor() {
+    this.canvas = document.getElementById("canvas")
+    this.ctx = canvas.getContext("2d")
+  }
 
   clearCanvas () {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
